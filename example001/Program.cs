9e2ca1a29d1fb56a2d426328ae55952e7152c5a0
unknown <inessalub@gmail.com>
2022-10-13 14:05:02 +0400

    example 001

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Найти кубы чисел от 1 до N
+
+int i = 1;
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()?? "0");
+
+for (i = 1; i <= N; i++)
+{
+    Console.WriteLine(i * i * i);
+}
