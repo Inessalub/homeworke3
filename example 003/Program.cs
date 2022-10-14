a460b7d5b6aed7b96e2da5f622fb98982c2e1948
unknown <inessalub@gmail.com>
2022-10-14 14:52:33 +0400

    example 004

--- example 003/Program.cs
+++ example 003/Program.cs
@@ -1,13 +1,13 @@
 ﻿//Написать программу вычисления произведения чисел от 1 до N
-//Показать кубы чисел, заканчивающихся на четную цифру
-
-
 
 Console.WriteLine("Введите число N: ");
 int N = int.Parse(Console.ReadLine()?? "0");
 int i = 1;
+int f = 1;
 
-for (i = 1; i <= N; i++)
+while (i <= N )
 {
-    Console.WriteLine(i * i * i);
+    f = f * i;
+    i++;
 }
+Console.WriteLine(f);
